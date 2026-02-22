37a22b88c0f9a90d9e6195f44c303f00903e747a
BryantBeltran <bel21030@byui.edu>
2026-02-21 18:55:57 -0700

    feat: Enable core library desugaring and add dependency for desugar_jdk_libs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -100,6 +101,8 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
 
